cf39badae9e5561a1d15c99e8b05f1024153facf
Nauryzbayev Darmanbek <n.darman05@gmail.com>
2019-12-10 20:41:38 +0600

    Routing

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,18 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/contacts', function(){
+    return "<h1>All contacts</h1>";
+})->name('contacts.index');
+
+Route::get('/contacts/create', function(){
+    return "<h1>Add new contact</h1>";
+})->name('contacts.create');
+
+
+Route::get('/contacts/{id}', function($id){
+    return App\Models\Contact::find($id);
+})->name('contacts.show');

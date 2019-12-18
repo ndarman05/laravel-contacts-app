f6abc3a2f5cfcce4cb064b676ea17411cdbd7b90
Nauryzbayev Darmanbek <n.darman05@gmail.com>
2019-12-18 13:37:50 +0600

    lesson27

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -11,4 +11,20 @@ class Contact extends Model
     public function company(){
         return $this->belongsTo(Company::class);
     }
+
+    public function scopeLatestFirst($query){
+        return $query->orderBy('id', 'desc');
+    }
+
+    public function scopeFilter($query){
+        if($companyId = request('company_id')){
+            $query->where('company_id', $companyId);
+        }
+
+        if($search = request('search')){
+            $query->where('first_name', 'LIKE', "%{$search}%");
+        }
+
+        return $query;
+    }
 }
